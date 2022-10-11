fa556c052002d85b8c65b659a0d186c5cd97ad75
Aaron Bembenek <aaronbem@amazon.com>
2022-10-11 14:57:02 -0400

    Resolve some relative paths.

--- kani-compiler/src/stubbing/annotations.rs
+++ kani-compiler/src/stubbing/annotations.rs
@@ -1,12 +1,18 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+use rustc_ast::Attribute;
 use rustc_data_structures::fx::FxHashMap;
 use rustc_driver::RunCompiler;
 use rustc_driver::{Callbacks, Compilation};
 use rustc_errors::ErrorGuaranteed;
+use rustc_hir::def::{DefKind, Res};
+use rustc_hir::def_id::LocalDefId;
+use rustc_hir::{Item, ItemKind, Path};
 use rustc_interface::interface::Compiler;
 use rustc_interface::Queries;
+use rustc_middle::ty::TyCtxt;
+use rustc_span::Span;
 
 use crate::attributes::{extract_path_arguments, partition_kanitool_attributes};
 
@@ -30,9 +36,153 @@ struct AnnotationCollectorCallbacks {
     stub_mapping: FxHashMap<String, FxHashMap<String, String>>,
 }
 
+enum TryResolveUseResult {
+    NotResolved,
+    Fn(String),
+    //Mod(LocalDefId),
+}
+
 impl AnnotationCollectorCallbacks {
     pub fn new() -> AnnotationCollectorCallbacks {
         AnnotationCollectorCallbacks { stub_mapping: FxHashMap::default() }
+    }
+
+    /*
+    see if name is relative
+    if so, try to resolve it
+    if this fails or it is not relative, then do absolute search
+    (first resolving self, super, etc.)
+    */
+    fn try_resolve(
+        tcx: TyCtxt,
+        span: Span,
+        name: &str,
+        current_module: LocalDefId,
+    ) -> Option<String> {
+        let path: Vec<&str> = name.split("::").collect();
+        if AnnotationCollectorCallbacks::is_relative_path(&path) {
+            let maybe_resolution =
+                AnnotationCollectorCallbacks::try_resolve_relative_path(tcx, current_module, &path);
+            if maybe_resolution.is_some() {
+                return maybe_resolution;
+            }
+        }
+        tcx.sess.span_err(span, format!("kani::stub_by: unable to resolve {}", name));
+        None
+    }
+
+    fn try_resolve_relative_path(
+        tcx: TyCtxt,
+        current_module: LocalDefId,
+        path: &Vec<&str>,
+    ) -> Option<String> {
+        let name = path.join("::");
+        let module_name = tcx.def_path_str(current_module.to_def_id());
+        let qualified_name =
+            if module_name.is_empty() { name.clone() } else { module_name + "::" + &name };
+
+        for item_id in tcx.hir().module_items(current_module) {
+            let item = tcx.hir().item(item_id);
+            println!("TRYING TO RESOLVE: {} aka {}", name, qualified_name);
+            match item.kind {
+                ItemKind::Fn(..) => {
+                    let fn_name = tcx.def_path_str(item.def_id.to_def_id());
+                    if *qualified_name == fn_name {
+                        println!("RESOLVED: {} --> {}", &name, fn_name);
+                        return Some(fn_name);
+                    }
+                }
+                ItemKind::Use(use_path, _) => {
+                    let maybe_resolved =
+                        AnnotationCollectorCallbacks::try_resolve_use(tcx, &name, item, use_path);
+                    match maybe_resolved {
+                        TryResolveUseResult::Fn(func) => return Some(func),
+                        _ => (),
+                    }
+                }
+                _ => (),
+            }
+            println!("ITEM {}", tcx.def_path_str(item.def_id.to_def_id()));
+            println!("{:#?}", item);
+        }
+        None
+    }
+
+    fn try_resolve_use(
+        tcx: TyCtxt,
+        name: &str,
+        item: &Item,
+        use_path: &Path,
+    ) -> TryResolveUseResult {
+        if let Res::Def(def_kind, def_id) = use_path.res {
+            match def_kind {
+                DefKind::Fn => {
+                    let ident = item.ident.to_string();
+                    println!("IDENT {}", ident);
+                    if ident == name {
+                        let res_name = tcx.def_path_str(def_id);
+                        println!("RESOLVED: {} --> {}", &name, res_name);
+                        return TryResolveUseResult::Fn(res_name);
+                    }
+                }
+                _ => {}
+            }
+        }
+        TryResolveUseResult::NotResolved
+    }
+
+    fn is_relative_path(_path: &Vec<&str>) -> bool {
+        return true;
+    }
+
+    fn extract_stub_by(
+        tcx: TyCtxt,
+        attr: &Attribute,
+        current_module: LocalDefId,
+    ) -> Option<(String, String)> {
+        if let Some(args) = extract_path_arguments(attr) {
+            if args.len() == 2 {
+                let original = AnnotationCollectorCallbacks::try_resolve(
+                    tcx,
+                    attr.span,
+                    &args[0],
+                    current_module,
+                );
+                let replacement = AnnotationCollectorCallbacks::try_resolve(
+                    tcx,
+                    attr.span,
+                    &args[1],
+                    current_module,
+                );
+                return original
+                    .map(|original| replacement.map(|replacement| (original, replacement)))
+                    .flatten();
+            }
+        }
+        tcx.sess.span_err(attr.span, "kani::stub_by takes two path arguments");
+        None
+    }
+
+    fn update_stub_mapping(
+        tcx: TyCtxt,
+        current_module: LocalDefId,
+        attr: &Attribute,
+        stub_pairs: &mut FxHashMap<String, String>,
+    ) {
+        if let Some((original, replacement)) =
+            AnnotationCollectorCallbacks::extract_stub_by(tcx, attr, current_module)
+        {
+            let other = stub_pairs.insert(original.clone(), replacement.clone());
+            if let Some(other) = other {
+                tcx.sess.span_err(
+                    attr.span,
+                    format!(
+                        "duplicate stub mapping: {} mapped to {} AND {}",
+                        original, replacement, other
+                    ),
+                );
+            }
+        }
     }
 }
 
@@ -43,37 +193,37 @@ impl Callbacks for AnnotationCollectorCallbacks {
         queries: &'tcx Queries<'tcx>,
     ) -> Compilation {
         queries.global_ctxt().unwrap().peek_mut().enter(|tcx| {
+            /*
+            for local_def_id in tcx.hir_crate_items(()).definitions().into_iter() {
+                println!("DEFINITION: {}", tcx.def_path_str(local_def_id.to_def_id()));
+            }
+            for foreign_item_id in tcx.hir_crate_items(()).foreign_items().into_iter() {
+                println!("FOREIGN ITEM: {}", tcx.def_path_str(foreign_item_id.def_id.to_def_id()));
+            }
+            */
             for item in tcx.hir_crate_items(()).items() {
                 let def_id = item.def_id.to_def_id();
+                //println!("ITEM: {}", tcx.def_path_str(def_id));
+                //println!("{:#?}", tcx.hir().item(item));
                 let (proof, other) = partition_kanitool_attributes(tcx.get_attrs_unchecked(def_id));
                 if proof.is_empty() {
                     continue;
                 }
                 let mut stub_pairs = FxHashMap::default();
+                let current_module =
+                    tcx.parent_module(tcx.hir().local_def_id_to_hir_id(item.def_id));
                 for (name, attr) in other {
                     if name == "stub_by" {
-                        if let Some(args) = extract_path_arguments(attr) {
-                            if args.len() == 2 {
-                                let other = stub_pairs.insert(args[0].clone(), args[1].clone());
-                                if let Some(other) = other {
-                                    tcx.sess.span_err(
-                                        attr.span,
-                                        format!(
-                                            "duplicate stub mapping: {} mapped to {} AND {}",
-                                            args[0], args[1], other
-                                        ),
-                                    );
-                                }
-                            } else {
-                                tcx.sess
-                                    .span_err(attr.span, "kani::stub_by takes two path arguments");
-                            }
-                        } else {
-                            tcx.sess.span_err(attr.span, "kani::stub_by takes two path arguments");
-                        }
+                        AnnotationCollectorCallbacks::update_stub_mapping(
+                            tcx,
+                            current_module,
+                            attr,
+                            &mut stub_pairs,
+                        );
                     }
                 }
-                self.stub_mapping.insert(tcx.def_path_str(def_id), stub_pairs);
+                let harness_name = tcx.def_path_str(def_id);
+                self.stub_mapping.insert(harness_name, stub_pairs);
             }
             tcx.sess.abort_if_errors();
             Compilation::Stop
